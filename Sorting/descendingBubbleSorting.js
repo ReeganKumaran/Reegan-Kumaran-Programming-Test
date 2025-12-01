7fd2f71c36d07b79d480f19ab380c2c5d3252481
Reegan <reegank20@gmail.com>
2025-12-02 01:32:38 +0530

    Descending Bubble Sorting

--- Sorting/descendingBubbleSorting.js
+++ Sorting/descendingBubbleSorting.js
@@ -1,8 +1,8 @@
 const bubbleSorting = (arr) => {
   const temp = [...arr];
-  for (let i = 1; i < temp.length; i++) {
-    for (let j = 0; j < temp.length - i - 1; j++) {
-      if (temp[j] > temp[j + 1]) {
+  for (let i = 0; i < temp.length; i++) {
+    for (let j = 0; j < temp.length - i ; j++) {
+      if (temp[j] < temp[j + 1]) {
         const min = temp[j + 1];
         temp[j + 1] = temp[j];
         temp[j] = min;
@@ -11,4 +11,4 @@ const bubbleSorting = (arr) => {
   }
   return temp;
 };
-console.log(bubbleSorting([3, 5, 1, 2, 6, 7, 8]));
+console.log(bubbleSorting([6, 5, 3, 1, 2, 8, 7]));
