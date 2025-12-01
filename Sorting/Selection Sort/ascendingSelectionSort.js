e1b5ad439d1f0f411b1731bcba0f50f336908ba2
Reegan <reegank20@gmail.com>
2025-12-02 02:02:06 +0530

    Ascending Selection Sort

---
+++ Sorting/Selection Sort/ascendingSelectionSort.js
@@ -0,0 +1,20 @@
+const selectionSort = (arr) => {
+  const temp = [...arr];
+  for (let i = 0; i < temp.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < temp.length; j++) {
+      if (temp[j] < temp[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      const swap = temp[i];
+      temp[i] = temp[minIndex];
+      temp[minIndex] = swap;
+    }
+  }
+  return temp;
+};
+
+console.log(selectionSort([5, 4, 2, 6, 1, 3]));
+// [5 ,4 ,2 ,6 ,1 ,3]
